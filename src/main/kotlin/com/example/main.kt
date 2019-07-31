29f58cdf7244d78de58552b09851d7e7715c1d26
sebastian.schmidt <bastman77@gmail.com>
2019-07-31 11:51:19 +0200

    swagger

--- src/main/kotlin/com/example/main.kt
+++ src/main/kotlin/com/example/main.kt
@@ -5,24 +5,38 @@ import io.javalin.http.Context
 import io.javalin.plugin.openapi.OpenApiOptions
 import io.javalin.plugin.openapi.OpenApiPlugin
 import io.javalin.plugin.openapi.annotations.OpenApi
+import io.javalin.plugin.openapi.annotations.OpenApiRequestBody
+import io.javalin.plugin.openapi.annotations.OpenApiResponse
+import io.javalin.plugin.openapi.dsl.document
+import io.javalin.plugin.openapi.dsl.documented
 import io.javalin.plugin.openapi.ui.ReDocOptions
 import io.javalin.plugin.openapi.ui.SwaggerOptions
 import io.swagger.v3.oas.models.info.Info
 
+data class User(val firstname:String)
 
 fun main(args: Array<String>) {
+
+
     val app = Javalin.create { config ->
         config.defaultContentType = "application/json"
         config.addStaticFiles("/public")
         config.enableCorsForAllOrigins()
         config.registerPlugin(OpenApiPlugin(getOpenApiOptions()));
+
+
     }
     app.get("/health") { ctx -> ctx.json(HealthResponse()) }
     app.get("/") { ctx -> ctx.json(IndexResponse()) }
 
 
+    val addUserDocs = document()
+            .body<User>()
+            .result<Unit>("400")
+            .result<Unit>("204")
 
     app.post("/users", UserController::createUser);
+    app.post("/users/v2", documented(addUserDocs, ::addUserHandlerV2))
 
     //OpenApiB
 
@@ -47,6 +61,27 @@ fun main(args: Array<String>) {
      */
 }
 
+fun addUserHandlerV2(ctx: Context) {
+    val user = ctx.body<User>()
+    //UserRepository.addUser(user)
+    ctx.status(204)
+}
+
+/*
+@OpenApi(
+        requestBody = OpenApiRequestBody(User::class),
+        responses = [
+            OpenApiResponse("400", Unit::class),
+            OpenApiResponse("201", Unit::class)
+        ]
+)
+fun addUserHandlerV3(ctx: Context) {
+    val user = ctx.body<User>()
+   // UserRepository.createUser(user)
+    ctx.status(201)
+}
+
+ */
 data class HealthResponse(val status: String = "UP")
 data class IndexResponse(val message: String = "It works ;)")
 
