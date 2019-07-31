7b1f931b188c05b6ab02127da5a7d9e8944bcaf4
sebastian.schmidt <bastman77@gmail.com>
2019-07-31 11:07:41 +0200

    swagger

--- src/main/kotlin/com/example/main.kt
+++ src/main/kotlin/com/example/main.kt
@@ -1,12 +1,17 @@
 package com.example
 
 import io.javalin.Javalin
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.swagger.v3.oas.models.info.Info
+
 
 fun main(args: Array<String>) {
     val app = Javalin.create { config ->
         config.defaultContentType = "application/json"
         config.addStaticFiles("/public")
         config.enableCorsForAllOrigins()
+        config.registerPlugin(OpenApiPlugin(getOpenApiOptions()));
     }
     app.get("/health")  { ctx -> ctx.json(HealthResponse()) }
     app.get("/")  { ctx -> ctx.json(IndexResponse()) }
@@ -20,3 +25,17 @@ fun main(args: Array<String>) {
 
 data class HealthResponse(val status:String="UP")
 data class IndexResponse(val message:String="It works ;)")
+
+private fun getOpenApiOptions(): OpenApiOptions {
+    val applicationInfo = Info()
+            .version("1.0")
+            .description("My Application")
+
+
+
+
+
+    return OpenApiOptions(applicationInfo).path("/swagger-docs")
+}
+
+
